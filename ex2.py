2b5f16105145e3743b3580daeba530a680263a4e
mafike <mafgenes1@icloud.com>
2023-01-03 15:55:24 -0800

    c2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+               translation += "G"
+        else:
+            translation += "g"
+    return translation
+print(translate(input("Enter a phrase: ")))                
---
+++ ex3.py
@@ -0,0 +1,5 @@
+try:
+    number = int(input("Enter a number: ")) #use try except
+    print(number)
+except:
+    print("Invalid number")
