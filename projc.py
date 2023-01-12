74f17a7be00096bdd511cde06da53050bacce279
mafike <mafgenes1@icloud.com>
2023-01-11 16:12:06 -0800

    c4

---
+++ projc.py
@@ -0,0 +1,32 @@
+class student:
+    def __init__(self, name, major, gpa, is_on_probagation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probagation = is_on_probagation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#number_grid = [
+     [1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9],
+     [0]
+#]
+#for row in number_grid:
+ #   for col in row:
+  #      print(col)
